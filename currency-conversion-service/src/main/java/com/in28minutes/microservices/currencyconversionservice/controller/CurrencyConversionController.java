a454879784897897537c2e5d510e993bfd6b7837
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-02-25 21:33:34 +0000

    currency-exchange-service & currency-exchange-service & netflix-zuul-api: added support to Sleuth and Bus amqp. They will be sending log messages with IDs (Sleuth) in a Zipkin format through RabbitMQ (used by default by Bus ampq)

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import com.in28minutes.microservices.currencyconversionservice.model.CurrencyCon
 import com.in28minutes.microservices.currencyconversionservice.services.CurrencyService;
 import com.in28minutes.microservices.currencyconversionservice.services.feign.CurrencyExchangeServiceProxy;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 /**
  * @author Alessandro Arosio - 24/02/2020 17:28
  */
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class CurrencyConversionController {
@@ -25,6 +27,7 @@ public class CurrencyConversionController {
                                               @PathVariable String to,
                                               @PathVariable BigDecimal quantity) {
         CurrencyConversion currency = currencyService.getCurrency(from, to);
+        log.info("{}", currency);
         return CurrencyConversion.builder()
                 .id(currency.getId())
                 .from(from)
--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -3,6 +3,7 @@ package com.in28minutes.microservices.currencyexchangeservice.controllers;
 import com.in28minutes.microservices.currencyexchangeservice.model.ExchangeValue;
 import com.in28minutes.microservices.currencyexchangeservice.repository.ExchangeValueRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author Alessandro Arosio - 23/02/2020 09:30
  */
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class CurrencyExchangeController {
@@ -22,6 +24,7 @@ public class CurrencyExchangeController {
     public ExchangeValue retrieveExchangeValue(@PathVariable String to, @PathVariable String from) {
         ExchangeValue result = exchangeValueRepository.findByFromAndTo(from, to);
         result.setPort(Integer.valueOf(environment.getProperty("local.server.port")));
+        log.info("{}", result);
         return result;
     }
 }
