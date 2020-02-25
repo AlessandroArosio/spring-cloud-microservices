056b0653275ad350a99b13bdd41b1a9893be9046
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-02-25 20:19:04 +0000

    currency-conversion-service and currency-exchange-service now are communicating through Zuul

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/services/feign/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/services/feign/CurrencyExchangeServiceProxy.java
@@ -9,11 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 /**
  * @author Alessandro Arosio - 25/02/2020 17:36
  */
-//@FeignClient(name = "currency-exchange-service", url = "localhost:8000") // removing hardcoded value
-@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     CurrencyConversion retrieveExchangeValue(@PathVariable("to") String to, @PathVariable("from") String from);
 }
