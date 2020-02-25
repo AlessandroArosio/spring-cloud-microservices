f16bed7198d0d49258d8504ba270c81752eef29f
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-02-25 18:15:38 +0000

    currency-conversion-service: added Ribbon load balancer and list of services in application.properties

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients("com.in28minutes.microservices.currencyconversionservice")
+@EnableFeignClients
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
 
